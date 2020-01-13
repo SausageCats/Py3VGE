83e809c3e43eef3f540e4e26deac0bb0af08328c
SausageCats <sausagecat0@gmail.com>
2020-01-14 00:04:24 +0900

    Change versions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='VGE',
-      version='2.0.0',
+      version='2.1.0',
       description='Virtual Grid Engine for running bioinformatics pipelines on MPI-base supercomputers',
       author='Satoshi ITO, Masaaki YADOME, and Satoru MIYANO',
       author_email='sito.public@gmail.com',
